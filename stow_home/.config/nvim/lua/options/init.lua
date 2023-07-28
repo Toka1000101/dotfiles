1ee24d6f9522a66322f3a299b1344a1264d3a803
Toka1000101 <tmaridashvili@protonmail.com>
2023-07-28 13:05:11 +0400

    add changes

--- stow_home/.config/nvim/lua/options/init.lua
+++ stow_home/.config/nvim/lua/options/init.lua
@@ -1,7 +1,19 @@
 local o = vim.opt
+local km = vim.keymap
+
+-- GENERAL 
 o.relativenumber = true
+o.cursorline = true
+o.autoread = true
+o.autowrite = true
+
+-- TAB SPACING 
 o.shiftwidth = 2
+o.shiftround = true
 o.tabstop = 2
+
+-- SEARCH OPTIONS
 o.ignorecase = true
 o.smartcase = true
-o.hlsearch = false
+
+
