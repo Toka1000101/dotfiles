a1debfcd61e95d3502b7f063f562d72659d16527
Toka1000101 <tmaridashvili@protonmail.com>
2023-08-08 19:41:38 +0400

    add relativenumber toggle, map hjkl to jkl;

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -1,10 +1,31 @@
 local map = vim.api.nvim_set_keymap
-
+local keymap = vim.keymap.set
+local g = vim.g
 local opts = {} 
 
 vim.g.mapleader = ' '
 
 -- telescope.nvim mappings
-map('n', '<leader>ff', ':Telescope find_files<CR>',opts)
-map('n', '<leader>fg', ':Telescope live_grep<CR>',opts)
-map('n', '<leader>ft', ':NvimTreeToggle<CR>',opts)
+keymap('n', '<leader>ff', ':Telescope find_files<CR>',opts)
+keymap('n', '<leader>fg', ':Telescope live_grep<CR>',opts)
+
+-- nvim-tree mappings
+keymap('n', '<leader>ft', ':NvimTreeToggle<CR>',opts)
+
+-- map hjkl to jkl; in normal and visual modes
+local modes = {'n','v'}
+keymap(modes,';','l',opts)
+keymap(modes, 'l', 'k',opts)
+keymap(modes, 'k', 'j',opts)
+keymap(modes, 'j', 'h',opts)
+
+-- shorcuts to change default options and options in options.lua
+g.isRelativeNumber = true
+
+function toggleRelativeNumber()
+	g.isRelativeNumber = not g.isRelativeNumber
+	vim.opt.relativenumber = not g.isRelativeNumber
+end
+
+keymap('n','<leader>rn', toggleRelativeNumber,opts)
+
