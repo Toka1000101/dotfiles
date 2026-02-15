1acb6a135d1671c94c30e7304a8a291f033cd5a7
Toka1000101 <tmardashvili@protonmail.com>
2026-02-14 22:42:57 -0500

    update i3lock.sh

--- i3/i3lock.sh
+++ i3/i3lock.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-TMPBG=/tmp/blurred_screen.png
-RES=$(xrandr | grep 'current' | sed -E 's/.*current\s([0-9]+)\sx\s([0-9]+).*/\1x\2/')
- 
-ffmpeg -f x11grab -video_size $RES -y -i $DISPLAY -filter_complex "boxblur=5:1" -vframes 1 $TMPBG -loglevel quiet
-i3lock -i $TMPBG
-rm $TMPBG
+i3lock -i ~/.config/dotfiles/i3/gae.png
 # i3lock sets keyboard to defaults, so this line is to set it back to original binding
 setxkbmap -layout us,ge -option caps:swapescape
