992a45a254548d648ea254cfa3d35dfd315e246b
Toka1000101 <tmardashvili@protonmail.com>
2026-04-17 23:56:32 -0400

    update treesitter

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,13 +1,20 @@
 return {
 	'nvim-treesitter/nvim-treesitter',
 	build = ":TSUpdate",
-	config = function ()
-    local configs = require("nvim-treesitter.configs")
-    configs.setup({
-        ensure_installed = {},
-        sync_install = false,
-        highlight = { enable = true },
-        indent = { enable = true },
-      })
-  end
+	config = function()
+		local status_ok, ts_configs = pcall(require, "nvim-treesitter.configs")
+		if not status_ok then
+			return
+		end
+
+		ts_configs.setup({
+			ensure_installed = { "java", "lua", "vim", "vimdoc", "query", "cpp", "markdown", "markdown_inline" },
+			sync_install = false,
+			highlight = {
+				enable = true,
+				additional_vim_regex_highlighting = false,
+			},
+			indent = { enable = true },
+		})
+	end
 }
