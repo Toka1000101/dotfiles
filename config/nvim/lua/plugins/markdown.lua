b22ba31a75d2572c14d790e243f364cabc1d06b5
Toka1000101 <tmaridashvili@protonmail.com>
2026-09-22 19:41:32 -0400

    configure markdown.nvim

--- config/nvim/lua/plugins/markdown.lua
+++ config/nvim/lua/plugins/markdown.lua
@@ -1,9 +1,23 @@
 return {
   "iamcco/markdown-preview.nvim",
   cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-  build = "cd app && npm install",
+  ft = { "markdown" },
+  build = function()
+    vim.fn["mkdp#util#install"]()
+  end,
   init = function()
     vim.g.mkdp_filetypes = { "markdown" }
+    vim.g.mkdp_auto_close = 1
+    vim.g.mkdp_theme = "dark"
+    vim.g.mkdp_refresh_slow = 0
   end,
-  ft = { "markdown" },
+  keys = {
+    {
+      "<leader>mp",
+      "<cmd>MarkdownPreviewToggle<cr>",
+      desc = "Toggle Markdown Preview",
+      ft = "markdown",
+    },
+  },
 }
+
