e352e9eb5ad77f08cf37ebfec73d38088a8f7206
Toka1000101 <tmaridashvili@protonmail.com>
2026-09-23 00:00:23 -0400

    use OXY2DEV/markview.nvim

--- config/nvim/lua/plugins/markdown.lua
+++ config/nvim/lua/plugins/markdown.lua
@@ -1,23 +1,17 @@
 return {
-  "iamcco/markdown-preview.nvim",
-  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-  ft = { "markdown" },
-  build = function()
-    vim.fn["mkdp#util#install"]()
-  end,
-  init = function()
-    vim.g.mkdp_filetypes = { "markdown" }
-    vim.g.mkdp_auto_close = 1
-    vim.g.mkdp_theme = "dark"
-    vim.g.mkdp_refresh_slow = 0
-  end,
-  keys = {
-    {
-      "<leader>mp",
-      "<cmd>MarkdownPreviewToggle<cr>",
-      desc = "Toggle Markdown Preview",
-      ft = "markdown",
-    },
-  },
-}
+    "OXY2DEV/markview.nvim",
+    lazy = false,
+		blocks = {
+			enable = true,
+
+			hl = "MarkviewCode",
+			pad_char = " ",
+			pad_amount = 3,
+
+			text = "  LaTeX ",
+			text_hl = "MarkviewCodeInfo"
+		},
+    -- Completion for `blink.cmp`
+    -- dependencies = { "saghen/blink.cmp" },
+};
 
